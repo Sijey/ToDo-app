94196737eda5f293baafc4a910b60fdf32b03124
Sijey <Popov040391@gmail.com>
2019-11-12 12:53:21 +0200

    Updates

---
+++ precache-manifest.9225cd68d0de67685b55500e5a2c2fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d0eb3904b6382307db8cb3f130a9fe",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "4f8d792e9a4c705b4d35",
+    "url": "/react_todo-app/static/css/main.b8bf75ff.chunk.css"
+  },
+  {
+    "revision": "d07772137980457e78fd",
+    "url": "/react_todo-app/static/js/2.3e72d70c.chunk.js"
+  },
+  {
+    "revision": "4f8d792e9a4c705b4d35",
+    "url": "/react_todo-app/static/js/main.ffc4e571.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
